b9beffae64cfcffc10889524c37f50a1def0c8ee
Arif9084332476 <203461328+Arif9084332476@users.noreply.github.com>
2026-03-12 19:55:12 +0530

    Update auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -41,7 +41,7 @@ const emailTransporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
- });
+});
 
 // ── POST /api/auth/send-otp ───────────────────────────────────
 // Request body: { email: "user@example.com" }
